a4dac891e44b841ca17f66888b94d3b5ae83d941
changmin Lee <lcmini6528@gmail.com>
2018-05-03 02:40:50 +0900

    Color, Font 추가

---
+++ DeliciousBody/DBColors.swift
@@ -0,0 +1,19 @@
+//
+//  DBColors.swift
+//  DeliciousBody
+//
+//  Created by changmin lee on 2018. 5. 3..
+//  Copyright © 2018년 changmin. All rights reserved.
+//
+import UIKit
+
+
+extension UIColor {
+    
+    @nonobjc class var themeBlue: UIColor {
+        return UIColor(red: 66.0 / 255.0, green: 72.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
+    }
+    
+    
+}
+
