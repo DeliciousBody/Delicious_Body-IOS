eec645f85888bec429acf2419dca32c90f4cb22d
changmin Lee <lcmini6528@gmail.com>
2018-05-27 01:02:05 +0900

    Subclass 추가

---
+++ DeliciousBody/ETC/DBViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DBViewController.swift
+//  DeliciousBody
+//
+//  Created by changmin lee on 2018. 5. 27..
+//  Copyright © 2018년 changmin. All rights reserved.
+//
+
+import UIKit
+
+class DBViewController: UIViewController {
+    @IBAction func back(segue: UIStoryboardSegue) {}
+
+}
