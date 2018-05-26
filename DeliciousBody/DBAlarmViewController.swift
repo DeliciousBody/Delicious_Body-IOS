fe95c9330cdbe621a8c7d7d09adda92c0abada05
changmin Lee <lcmini6528@gmail.com>
2018-05-26 22:40:14 +0900

    알람뷰 구현

--- DeliciousBody/DBAlarmViewController.swift
+++ DeliciousBody/DBAlarmViewController.swift
@@ -11,6 +11,7 @@ import RangeSeekSlider
 
 class DBAlarmViewController: UIViewController {
 
+    @IBOutlet weak var view_Weekend: UIView!
     @IBOutlet weak var slider_Weekend: RangeSeekSlider!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,5 +25,36 @@ class DBAlarmViewController: UIViewController {
         slider_Weekend.maxLabelFont = UIFont.sliderLabelFont
         
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        UIApplication.shared.statusBarStyle = .lightContent
+    }
 
+    @IBAction func masterAlarmOnOff(_ sender: UISwitch) {
+        let duration = 0.15
+        if sender.isOn {
+            view_Weekend.isHidden = false
+            UIView.animate(withDuration: duration
+                , animations: { [weak self] in
+                    self?.view_Weekend.alpha = 1.0
+            })
+        } else {
+            UIView.animate(withDuration: duration, animations: { [weak self] in
+                self?.view_Weekend.alpha = 0.0
+            }) { [weak self] (_)  in
+                self?.view_Weekend.isHidden = true
+            }
+        }
+        
+        
+    }
+    
+    
+    @IBAction func optionAlarmOnOff(_ sender: UISwitch) {
+        
+        slider_Weekend.colorBetweenHandles = sender.isOn ? UIColor.themeBlue : UIColor.subGray216
+
+        slider_Weekend.layoutSubviews()
+    }
+    
 }
