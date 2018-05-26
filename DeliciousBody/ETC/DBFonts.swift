829d3f2715e04b4a493273e0df1175e28de5a5f0
changmin Lee <lcmini6528@gmail.com>
2018-05-26 22:40:39 +0900

    유틸 추가

---
+++ DeliciousBody/ETC/DBFonts.swift
@@ -0,0 +1,22 @@
+//
+//  DBFonts.swift
+//  DeliciousBody
+//
+//  Created by changmin lee on 2018. 5. 3..
+//  Copyright © 2018년 changmin. All rights reserved.
+//
+import UIKit
+extension UIFont {
+    
+    class var caption1Black: UIFont {
+        return UIFont.systemFont(ofSize: 12.0, weight: .regular)
+    }
+    
+    class var sliderLabelFont: UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Bold", size: 12.0)!
+    }
+    class var textFieldFont: UIFont {
+        return UIFont(name: "AppleSDGothicNeo-SemiBold", size: 15.0)!
+    }
+    
+}
