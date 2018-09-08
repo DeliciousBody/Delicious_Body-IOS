40041bf7cc4953d5a8e5a144176a171366e8003d
changmin Lee <lcmini6528@gmail.com>
2018-09-09 00:28:57 +0900

    Image 추가

---
+++ DeliciousBody/CustomViews/DBRecordCell.swift
@@ -0,0 +1,13 @@
+//
+//  DBRecordCell.swift
+//  DeliciousBody
+//
+//  Created by changmin lee on 2018. 9. 8..
+//  Copyright © 2018년 changmin. All rights reserved.
+//
+
+import UIKit
+
+class DBRecordCell: UICollectionViewCell {
+    
+}
